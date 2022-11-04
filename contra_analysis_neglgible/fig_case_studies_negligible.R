6389dc1925dce156c7b47b59fab7b8ac7e322f20
bacorliss <44813189+bacorliss@users.noreply.github.com>
2022-11-03 22:46:03 -0400

    Both applied examples working for neglgible.

--- contra_analysis_neglgible/fig_case_studies_negligible.R
+++ contra_analysis_neglgible/fig_case_studies_negligible.R
@@ -23,17 +23,13 @@ df_chol <- read_csv(file.path(proj_path, base_dir, "chol_results.csv"))
 df_chol$study <- abbreviate(df_chol$study,6)
 df_chol$ICV <- df_chol$mean_x /(df_chol$s_x/sqrt(df_chol$n_x))
 
-
-
+# Interval estimations of relative difference in means
 df_interval <- calculate_contra_stats(df_chol)
 
-
 # Contra-plot
-source("R/contra_plot.R")
 df_contra <- cbind(df_chol[c("study_id", "ctrl", "tx", "sp", "year", "study")],
                    df_interval[c("int_estimate", "int_lower", "int_upper", "rldm","rmdm")])
 df_contra <- subset(df_contra, rldm < 200)
-
 contra_plot(df = df_contra, col_x_pos = "auto",
             xlabel = "Relative Difference in Means", plot_title = "Total Plasma Cholesterol",
             ggsize = c(3.5, 6.5), fig_path = fig_path, fig_name = "Chol(-)_rel_conf_closer_contra_plot.png",
@@ -42,17 +38,6 @@ contra_plot(df = df_contra, col_x_pos = "auto",
             cum_col_x_pos_adj = c(0, .04, 0, -.01, 0.04, -.02, 0))
 
 
-# least_col rldm
-# most_col rmdm
-
-
-
-
-contra_plot(df = subset(df_contra, upper > 0 ), sort_colname = "closest", col_x_pos = "auto", 
-            xlabel = "Relative Difference in Means", plot_title = "Total Plasma Cholesterol",
-            ggsize = c(3.5, 10), fig_path = fig_path, fig_name = "Chol(+)_rel_conf_closer_contra_plot.png",
-            tf_xlims = c(0, 5), relative = TRUE, estimate_colname = "closest", estimate_label = '"Ls%"',
-            cum_col_x_pos_adj = c(0, -.01, 0, 0.04, .02, 0))
 
 
 
@@ -63,23 +48,20 @@ df_plaq <- read_csv(file.path(proj_path, base_dir, "plaq_results.csv"))
 df_plaq$study <- abbreviate(df_plaq$study,6)
 df_plaq$ICV <- df_plaq$mean_x /(df_plaq$s_x/sqrt(df_plaq$n_x))
 
-conf_ints_list = list()
-for (n in 1:nrow(df_plaq)) {
-  conf_ints_list[[n]] <-
-    norm_credint_dm(df_plaq$mean_x[n], df_plaq$s_x[n], df_plaq$n_x[n],
-                        df_plaq$mean_y[n], df_plaq$s_y[n], df_plaq$n_y[n],
-                        conf.level = 0.95, relative = TRUE)
-}; conf_ints_list <- do.call(rbind,conf_ints_list)
+# Interval estimations of relative difference in means
+df_interval <- calculate_contra_stats(df_chol)
 
-df_conf_ints <- as.data.frame(matrix(unlist(conf_ints_list), ncol = ncol(conf_ints_list),
-                                     dimnames = list(NULL, colnames(conf_ints_list))))
 # Contra-plot
-df_contra <- cbind(df_plaq[c("study_id","ctrl", "tx", "sp", "year", "study")],
-                   df_conf_ints[c("estimate", "lower", "upper")])
+df_contra <- cbind(df_chol[c("study_id", "ctrl", "tx", "sp", "year", "study")],
+                   df_interval[c("int_estimate", "int_lower", "int_upper", "rldm","rmdm")])
 contra_plot(df = df_contra, col_x_pos = "auto",
             xlabel = "Relative Difference in Means", plot_title = "Plaque Area",
             ggsize = c(3.5, 6.5), fig_path = fig_path, fig_name = "Plaq(-)_rel_conf_closer_contra_plot.png",
-            tf_xlims = c(-0.95, 0), relative = TRUE, estimate_colname = "closest", estimate_label = '"Ls%"',
-            cum_col_x_pos_adj = c(0, -.01, 0, 0.04, .02, 0))
+            tf_xlims = c(-0.95, 10), relative = TRUE, least_colname = c("rldm",'"Ls%"'),
+            most_colname = c("rmdm",'"Ms%"'),
+            cum_col_x_pos_adj = c(0, .04, 0, -.01, 0.04, -.02, 0))
+
+
+
 
 
